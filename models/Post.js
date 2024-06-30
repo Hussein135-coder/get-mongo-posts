fd78db760006bba07318c1e41df9df7c1424697f
Hussein <husseinsym3@gmail.com>
2024-06-30 22:41:39 +0300

    add _id

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  content: { type: String, required: true },
-  link: { type: String, required: true },
-  date: { type: Date, required: true },
-});
+const PostSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    content: { type: String, required: true },
+    link: { type: String, required: true },
+    date: { type: Date, required: true },
+  },
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("Post", PostSchema);
