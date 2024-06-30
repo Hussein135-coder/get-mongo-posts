a8a5665db36c713185c1de5709473ec65078f99c
Hussein <husseinsym3@gmail.com>
2024-06-30 18:56:56 +0300

    modify socket

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const { Server } = require("socket.io");
-
-const io = new Server({});
-
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 const mongoose = require("mongoose");
-require("dotenv").config();
 const Post = require("./models/Post");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 8000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 2000;
 const MONGO_URI = process.env.MONGO;
+
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
@@ -18,7 +22,6 @@ io.on("connection", (socket) => {
 
   const fetchPosts = async () => {
     try {
-      console.log("get Posts");
       const posts = await Post.find().sort({ date: -1 }).limit(100);
       console.log(posts);
       socket.emit("posts", posts);
@@ -36,4 +39,4 @@ io.on("connection", (socket) => {
   });
 });
 
-io.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
